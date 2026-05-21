4e65d8661b5a9cad224ac9ca734b4d2597310ad2
CrispyRan <158082433+CrispyRan@users.noreply.github.com>
2026-05-21 12:34:29 +0800

    fix: 手机端功能菜单圆卡片弹出高度限制改为80vh+可滚动

--- view/src/layout/components/ma-buttonMenu.vue
+++ view/src/layout/components/ma-buttonMenu.vue
@@ -11,7 +11,7 @@
         <icon-menu v-else />
       </div>
       <template #content>
-        <a-menu mode="popButton" showCollapseButton :popup-max-height="360">
+        <a-menu mode="popButton" showCollapseButton :popup-max-height="isMobile ? '80vh' : 360">
           <children-menu v-model="userStore.routers" />
         </a-menu>
       </template>
@@ -20,16 +20,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useAppStore, useUserStore } from '@/store'
 
 import ChildrenMenu from './components/children-menu.vue'
 
 const userStore = useUserStore()
 const popupVisible = ref(false)
+const isMobile = computed(() => window.innerWidth < 768)
 
 </script>
 
 <style scoped>
-
+.button-menu :deep(.arco-menu-pop-button) {
+  max-height: 80vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
 </style>
