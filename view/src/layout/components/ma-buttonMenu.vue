8c3865e1d129d58f50e0102385f992cdd38acd4f
CrispyRan <158082433+CrispyRan@users.noreply.github.com>
2026-05-21 13:02:06 +0800

    Revert "fix: 恢复弹窗锚定，改用全局CSS控制手机菜单滚动"
    This reverts commit 6ff460e0911694afa3000949ee4faead0e76d054.

--- view/src/layout/components/ma-buttonMenu.vue
+++ view/src/layout/components/ma-buttonMenu.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="block lg:hidden button-menu">
+  <div class="block lg:hidden button-menu" ref="menuRef">
     <a-trigger
       :trigger="['click']"
       clickToClose
       position="top"
       v-model:popupVisible="popupVisible"
+      :popup-container="() => $refs.menuRef"
     >
       <div :class="`button-trigger ${popupVisible ? 'button-trigger-active' : ''}`">
         <icon-close v-if="popupVisible" />
@@ -26,10 +27,33 @@ import { useAppStore, useUserStore } from '@/store'
 import ChildrenMenu from './components/children-menu.vue'
 
 const userStore = useUserStore()
+const menuRef = ref(null)
 const popupVisible = ref(false)
 const isMobile = computed(() => window.innerWidth < 768)
 
 </script>
 
 <style scoped>
+.button-menu {
+  position: relative;
+  z-index: 100;
+}
+
+.button-menu :deep(.arco-trigger-popup) {
+  max-height: 80vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+
+.button-menu :deep(.arco-trigger-popup-content) {
+  max-height: 80vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+
+.button-menu :deep(.arco-menu-pop-button) {
+  max-height: 80vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
 </style>
